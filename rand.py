5910acc0d11af93d51db4a3f3a619d4f8f72dfe1
vivek2550 <vp760480@gmail.com>
2024-08-10 20:27:05 +0530

    add new method

--- rand.py
+++ rand.py
@@ -1,4 +1,16 @@
 import random
 
-num = random.randint(1,100)
+# use Randint method
+print(random.randint(1, 10))
+
+
+list =[1,2,3,4,5]
+random.shuffle(list)
+print(list)
+
+
+# use random operation
+list =[1,2,3,4,5]
+num = random.choice(list)
 print(num)
+
