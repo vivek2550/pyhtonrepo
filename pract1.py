dfa2d34b2c5f277aee643340fc1e4db8d44d00b4
vivek2550 <vp760480@gmail.com>
2024-08-18 14:29:01 +0530

    mew constructor

---
+++ pract1.py
@@ -0,0 +1,12 @@
+class Student:
+    def __init__(self, name, grade):   
+        self.name = name
+        self.grade = grade
+
+
+s1 = Student("vivek",[45,54,99])
+
+print(s1.name)
+print(s1.grade)
+
+
