c38582ca855ac3d6c871264249f36c89d638aad3
vivek2550 <vp760480@gmail.com>
2024-08-20 23:34:45 +0530

    update new metthod

--- pract1.py
+++ pract1.py
@@ -1,3 +1,5 @@
+# Constructor 
+
 class Student:
     def __init__(self, name, grade):   
         self.name = name
@@ -9,4 +11,15 @@ s1 = Student("vivek",[45,54,99])
 print(s1.name)
 print(s1.grade)
 
+
+
+# static method
+
+class Student:
+    @staticmethod
+    def myfunction():
+        print("hello bacchon")
+
+obj = Student()
+obj.myfunction()
 
