405a78769717090a04698e355034322def3fbb59
vivek2550 <vp760480@gmail.com>
2024-09-29 03:09:22 +0530

    basic program

--- bpractice.py
+++ bpractice.py
@@ -10,6 +10,8 @@ print("the sum is",sum)
 
 # wap a program to print the hello world
 print("hello world")
+str = "hello world"
+print(str)
 
 # wap a program to calculate the sr root of the given num
 num = 64
@@ -27,4 +29,30 @@ b = 3
 h = 2
 area = (b*h)/2
 print(area)
+
+# Wap a program to swap the two values
+x =4
+y =3
+print("before swapping")
+print(x,y)
+print("after swapping")
+x = x+y
+y = x-y
+x = x-y
+print(x,y)
+
+# Wap the program to convert the kilometer into miles
+# 1km = 0.621371
+km = 10
+miles = km*0.621371
+print(miles)
+
+# Wap a program to check the num is pos, neg and zero
+num = int(input("enter the value"))
+if num>0:
+    print("num is positive")
+elif num ==0:
+    print("num is zero")
+else:
+    print("negative")    
 
