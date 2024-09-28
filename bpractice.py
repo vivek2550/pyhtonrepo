2af6832ca239f50e0f2866a8f8dfc51eae852569
vivek2550 <vp760480@gmail.com>
2024-09-29 02:40:43 +0530

    basic program

---
+++ bpractice.py
@@ -0,0 +1,30 @@
+# Wap the python program to add the two num.
+a = 4
+b = 6
+print(a+b)
+# user input
+a = float(input("enter the num"))
+b = float(input("enter the num"))
+sum = a+b
+print("the sum is",sum)
+
+# wap a program to print the hello world
+print("hello world")
+
+# wap a program to calculate the sr root of the given num
+num = 64
+ans = num**(1/2)
+print("the sq root of the num is",ans)
+
+# By using math Module
+import math
+num = 81
+ans = math.sqrt(num)
+print("the sq root of the num is",ans)
+
+# Wap a program to calculate the area of the triangle 
+b = 3
+h = 2
+area = (b*h)/2
+print(area)
+
